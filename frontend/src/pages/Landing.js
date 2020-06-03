030fd6b16830681c5de99d8c0c9e74eb918170a7
Abdul <diazabdulm@gmail.com>
2020-06-03 18:22:30 -0400

    reworked landing ui and functions

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,48 +1,74 @@
-import React, { Fragment, useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import styled from "styled-components";
-import Container from "react-bootstrap/Container";
-
-import { selectUser } from "../reducers/authSlice";
 
 import withNavigation from "../hocs/withNavigation";
 
 import Introduction from "../components/Introduction";
 import PostItem from "../components/PostItem";
 
+const Container = styled.div`
+  max-width: 90%;
+  margin: 0 auto;
+  flex: 1 0 auto;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Intro = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 46px 0 60px 0;
+
+  & h1 {
+    margin: 0 15px 0 0;
+    font-weight: 500;
+  }
+
+  @media (max-width: 780px) {
+    flex-flow: column;
+  }
+`;
+
 function LandingPage() {
-  const user = useSelector(selectUser);
-  const [projectFeed, setProjectFeed] = useState([]);
+  const [feed, setFeed] = useState([]);
 
   useEffect(() => {
     let mounted = true;
-    const fetchProjecFeed = async () => {
-      const { data: feed } = await axios.get("/posts/all");
-      if (mounted) setProjectFeed(feed);
+    const fetchFeed = async () => {
+      if (!mounted) return;
+      const { data } = await axios.get("/posts/all");
+      setFeed(data);
     };
-    fetchProjecFeed();
+    fetchFeed();
     return () => (mounted = false);
   }, []);
 
   useEffect(() => {
-    document.title =
-      "Rumbbble - Discover the World's Top Developers and Programming Enthusiasts";
+    document.title = "Top Posts — Rumbbble";
   }, []);
 
-  const renderPostItems = () =>
-    projectFeed.map((props) => (
-      <PostItem key={props._id} {...props}>
-        <div>Hello</div>
-        <Introduction />
-      </PostItem>
-    ));
+  const renderPostItems = feed.map((props) => (
+    <PostItem key={props._id} {...props}>
+      <div>Hello</div>
+      <Introduction />
+    </PostItem>
+  ));
 
   return (
-    <Fragment>
-      {!user && <Introduction />}
-      <Container className="px-sm-0">{renderPostItems()}</Container>
-    </Fragment>
+    <Container>
+      <Intro>
+        <h1>Top programming projects sorted by</h1>
+        <select name="sort">
+          <option value="new">new</option>
+          <option value="likes">likes</option>
+          <option value="comments">comments</option>
+          <option value="oldest">oldest</option>
+        </select>
+      </Intro>
+      {renderPostItems}
+    </Container>
   );
 }
 
