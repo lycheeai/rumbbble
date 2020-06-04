43d423874823c972979e11bffc09990056109e08
Abdul <diazabdulm@gmail.com>
2020-06-04 00:48:49 -0400

    re-styled header buttons

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -37,21 +37,52 @@ const Avatar = styled.img`
   border-radius: 18px;
 `;
 
-const Button = styled(Link)``;
+const Button = styled(Link)`
+  border: 0;
+  border-radius: 10px;
+  font-size: 13px;
+  letter-spacing: 2px;
+  padding: 10px;
+  text-transform: uppercase;
+  text-align: center;
+  text-decoration: none;
+  background: #00b1ff;
+  color: #fff;
+  transition: background 300ms ease-in-out;
+  display: flex;
+
+  &:hover,
+  &:focus,
+  &:active {
+    background: #00aaf5;
+  }
+
+  & img {
+    width: 16px;
+    margin-right: 10px;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 12px;
+    letter-spacing: 1.8px;
+  }
+`;
 
 export default function Navigation() {
   const user = useSelector(selectUser);
 
   const renderLoggedIn = (
     <div>
+      <Button to="/projects/new" style={{ marginRight: "25px" }}>
+        + New Post
+      </Button>
       <Avatar src={user.picture} alt={user.name} />
-      <Button to="/projects/new">Upload</Button>
     </div>
   );
 
   const renderLoggedOut = (
     <div>
-      <Button to="/login">
+      <Button as="a" href="/auth/github">
         <img src={require("../assets/github-logo.png")} />
         Sign in/up
       </Button>
--- frontend/src/utilities/GlobalStyle.js
+++ frontend/src/utilities/GlobalStyle.js
@@ -1,6 +1,10 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
   body {
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
     background: #F4F4F4;
