fe115cd7b4792a5ab941f36954d448df93766f90
Abdul <diazabdulm@gmail.com>
2020-06-04 00:12:44 -0400

    removed references to login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,6 @@ import { fetchUser } from "./reducers/authSlice";
 import PrivateRoute from "./components/PrivateRoute";
 
 import LandingPage from "./pages/Landing";
-import LoginPage from "./pages/Login";
 import ProjectPage from "./pages/Project";
 import UploadPage from "./pages/Upload";
 import EditPage from "./pages/Edit";
@@ -23,7 +22,6 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={LandingPage} />
-        <Route exact path="/login" component={LoginPage} />
         <PrivateRoute exact path="/projects/new" component={UploadPage} />
         <Route exact path="/projects/:projectId" component={ProjectPage} />
         <PrivateRoute
