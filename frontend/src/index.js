8884b343d966a796fda2031c7de70aaa8bb17e3f
Abdul <diazabdulm@gmail.com>
2020-06-04 00:23:04 -0400

    removed bootstrap custom theme scss

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,6 @@ import App from "./App";
 import store from "./app/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
-import "./utilities/theme.scss";
 import "normalize.css";
 
 import { GlobalStyle } from "./utilities/GlobalStyle";
