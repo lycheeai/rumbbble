6721f6c22a7953d0559a8acb04ad9d2ec960d30c
Abdul <diazabdulm@gmail.com>
2020-06-03 23:59:58 -0400

    re-skinned commentform without bootstrap

--- frontend/src/components/CommentForm.js
+++ frontend/src/components/CommentForm.js
@@ -1,6 +1,31 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+
+const Container = styled.form`
+  display: flex;
+  align-items: center;
+`;
+
+const Content = styled.input`
+  flex: 1 0 auto;
+  margin-right: 20px;
+  font-size: 18px;
+  color: #292f3f;
+  height: 50px;
+  border: 0;
+  background: transparent;
+`;
+
+const Submit = styled.button`
+  border-radius: 50px;
+  font-size: 17px;
+  letter-spacing: normal;
+  text-transform: none;
+  padding: 6px 15px;
+  outline: none;
+`;
 
 export default function CommentForm({ projectId, submitComment }) {
   const [formData, setFormData] = useState("");
@@ -13,19 +38,16 @@ export default function CommentForm({ projectId, submitComment }) {
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group controlId="comment">
-        <Form.Label>Enter comment</Form.Label>
-        <Form.Control
-          as="textarea"
-          name="comment"
-          onChange={handleChange}
-          value={formData}
-        ></Form.Control>
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit comment
-      </Button>
-    </Form>
+    <Container onSubmit={handleSubmit}>
+      <Content
+        required
+        type="text"
+        name="comment"
+        placeholder="Write a comment..."
+        onChange={handleChange}
+        value={formData}
+      />
+      <Submit>Post</Submit>
+    </Container>
   );
 }
